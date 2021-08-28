070b2a843cba9ce08c8bde321f80133efbbb2ae4
pradow <pradochris+pradow@protonmail.com>
2021-08-28 09:38:15 -0700

    add newDog form to test out controlled inputs functionality

--- src/index.js
+++ src/index.js
@@ -44,6 +44,8 @@ const data = [
 function VideoList() {
   const [dogs, setDogs] = useState(data);
   const [fetchData, setFetchData] = useState([]);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputName, setInputName] = useState("");
   const getData = async () => {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const dataObject = await response.json();
@@ -60,8 +62,49 @@ function VideoList() {
 
     setDogs(filteredArray);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!inputName || !inputTitle) {
+      alert("please fill out all inputs");
+      return;
+    }
+
+    const submitedData = {
+      id: new Date().toString(),
+      title: inputTitle,
+      image: "https://placedog.net/500/280",
+      user: inputName,
+      views: "22k",
+      published: "9 months",
+      avatar: "https://placedog.net/210/167",
+    };
+
+    setDogs([...dogs, submitedData]);
+
+    setInputTitle("");
+    setInputName("");
+  };
   return (
     <div className="content">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="inputName">Name</label>
+        <input
+          value={inputName}
+          type="text"
+          id="inputName"
+          onChange={(e) => setInputName(e.target.value)}
+        />
+        <label htmlFor="inputTitle">Title</label>
+        <input
+          value={inputTitle}
+          type="text"
+          id="inputTitle"
+          onChange={(e) => setInputTitle(e.target.value)}
+        />
+        <button type="submit">add dog</button>
+      </form>
       <div className="current-user">
         <img src={fetchData.message} alt="A picture of a dog" />
       </div>
