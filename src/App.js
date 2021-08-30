ff7ad12e4b691d3115a829cd29594b1613d1234f
pradow <pradochris+pradow@protonmail.com>
2021-08-30 09:51:08 -0700

    incorporate react context api

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddForm from "./AddForm";
+import VideoList from "./VideoList";
+
+function App() {
+  return (
+    <>
+      <AddForm />
+      <VideoList />
+    </>
+  );
+}
+
+export default App;
