fc494df04ae9f07695f083579876d834dfe778e8
pradow <pradochris+pradow@protonmail.com>
2021-09-01 09:17:19 -0700

    incorporate react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddForm from "./AddForm";
 import VideoList from "./VideoList";
+import Nav from "./Nav";
+import About from "./About";
+import Error from "./Error";
 
 function App() {
   return (
-    <>
-      <AddForm />
-      <VideoList />
-    </>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <>
+            <AddForm />
+            <VideoList />
+          </>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
